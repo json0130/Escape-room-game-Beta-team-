8ad1ab375ca60d8166fd0556a2a28b1b2ecad092
Jay <json941@aucklanduni.ac.nz>
2023-09-14 23:14:36 +1200

    Created spaceship flying animation when button is clicked

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -43,8 +43,8 @@ public class App extends Application {
    */
   @Override
   public void start(final Stage stage) throws IOException {
-    Parent root = loadFxml("room");
-    scene = new Scene(root, 600, 470);
+    Parent root = loadFxml("start");
+    scene = new Scene(root, 1000, 650);
     stage.setScene(scene);
     stage.show();
     root.requestFocus();
