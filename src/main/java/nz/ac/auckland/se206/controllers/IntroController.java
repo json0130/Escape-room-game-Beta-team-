4ba4a939a2dc89ecf23da3197f9446af3c1404ab
Jay <json941@aucklanduni.ac.nz>
2023-09-17 02:11:13 +1200

    Can select different difficulty and game time and it will play the animation

--- src/main/java/nz/ac/auckland/se206/controllers/IntroController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/IntroController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.ParallelTransition;
 import javafx.animation.PathTransition;
 import javafx.animation.PauseTransition;
 import javafx.animation.RotateTransition;
@@ -14,35 +15,60 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.CubicCurveTo;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
 
 public class IntroController implements Initializable {
 
-    @FXML
-    private Button startButton;
-    @FXML
-    private ImageView spaceship;
-    @FXML
-    private Circle c1;
-    @FXML
-    private Circle c2;
-    @FXML
-    private Circle c3;
-    @FXML
-    private ImageView background;
-    @FXML
-    private ImageView r2;
-    @FXML
-    private ImageView r3;
+    @FXML private Button minB2;
+    @FXML private Button minB4;
+    @FXML private Button minB6;
+    @FXML private Button easyButton;
+    @FXML private Button mediumButton;
+    @FXML private Button hardButton;
+    @FXML private Button startButton;
+    @FXML private ImageView spaceship;
+    @FXML private ImageView background;
+    @FXML private ImageView background2;
+    @FXML private Rectangle easybox;
+    @FXML private Rectangle mediumbox;
+    @FXML private Rectangle hardbox;
+    @FXML private Label easy;
+    @FXML private Label medium;
+    @FXML private Label hard;
 
     private boolean animationStarted = false;
+    private boolean isLevelSelected = false;
+
+    @FXML
+    private void levelButtonClicked(){
+
+        isLevelSelected = true;
+
+        easybox.setVisible(false);
+        mediumbox.setVisible(false);
+        hardbox.setVisible(false);
+        easy.setVisible(false);
+        medium.setVisible(false);
+        hard.setVisible(false);
+
+        minB2.setVisible(true);
+        minB4.setVisible(true);
+        minB6.setVisible(true);
+    }
+
+    @FXML
+    private void minBClicked(){
+        startButton.setVisible(true);
+    }
 
     @FXML
     private void startAnimation() { 
@@ -87,6 +113,12 @@ public class IntroController implements Initializable {
         // }
 
         if (!animationStarted) {
+            spaceship.setVisible(true);
+            background2.setVisible(true);
+            minB2.setDisable(false);
+                minB4.setDisable(false);
+                minB6.setDisable(false);
+
             // Disable the button to prevent multiple clicks during animation
             //startButton.setDisable(true);
 
@@ -120,7 +152,7 @@ public class IntroController implements Initializable {
 
             // Enable the button when the animation is finished
             spaceshipPathTransition.setOnFinished(event -> {
-                startButton.setDisable(false);
+                System.out.println("Animation Starts");
                 animationStarted = false;
             });
 
@@ -128,56 +160,101 @@ public class IntroController implements Initializable {
         }
     }
 
+    @FXML
+private void buttonHovered(Button button, Label easy, Rectangle easybox) {
+    //TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), button);
+ if(!isLevelSelected) {
+    button.setOnMouseEntered(e -> {
+        // Move the button up for 0.2 seconds to -100
+        button.setTranslateY(-100);
+        
+        // Make "easy" label and "easybox" visible
+        easy.setVisible(true);
+        easybox.setVisible(true);
 
-
-
-    private void setRotate(Circle c, boolean reverse, int angle, int duration){
-        RotateTransition rt = new RotateTransition(Duration.seconds(duration), c);
-        rt.setAutoReverse(reverse);
-        rt.setByAngle(angle);
-        rt.setDelay(Duration.seconds(0));
-        rt.setRate(3);
-        rt.setCycleCount(18);
-        rt.play();
-    }
-
-    private void setMovement(ImageView r, boolean reverse, int duration, double endX, double endY, int delaySeconds) {
-    double startX = r.getTranslateX();
-    double startY = r.getTranslateY();
-
-    Path path = new Path();
-    path.getElements().add(new MoveTo(startX, startY));
-    path.getElements().add(new LineTo(endX, endY));
-
-    PathTransition pathTransition = new PathTransition();
-    pathTransition.setNode(r);
-    pathTransition.setPath(path);
-    pathTransition.setDuration(Duration.seconds(duration));
-    pathTransition.setCycleCount(reverse ? Animation.INDEFINITE : 1);
-    pathTransition.setAutoReverse(reverse);
-
-    SequentialTransition sequentialTransition = new SequentialTransition();
-    sequentialTransition.getChildren().addAll(
-        new PauseTransition(Duration.seconds(delaySeconds)),
-        pathTransition
-    );
-
-    sequentialTransition.setOnFinished(event -> {
-        // Reset the position of the ImageView to its original location
-        r.setTranslateX(startX);
-        r.setTranslateY(startY);
-
-        // Start the animation again
-        sequentialTransition.playFromStart();
+        // Start the translation animation
     });
 
-    sequentialTransition.play();
-    }
+    button.setOnMouseExited(e -> {
+        // Delay the hiding of elements by 3 seconds
+        PauseTransition pauseTransition = new PauseTransition(Duration.seconds(2.5));
+        pauseTransition.setOnFinished(event -> {
+            // Return the button to its original position
+            button.setTranslateY(0);
+            
+            // Hide "easy" label and "easybox"
+            easy.setVisible(false);
+            easybox.setVisible(false);
+
+            // Start the translation animation
+
+        });
+        pauseTransition.play();
+    });
+}
+}
+
+    // private void setMovement(ImageView r, boolean reverse, int duration, double endX, double endY, int delaySeconds) {
+    //     RotateTransition rt = new RotateTransition(Duration.seconds(duration), c);
+    //     rt.setAutoReverse(reverse);
+    //     //rt.setByAngle(angle);
+    //     rt.setDelay(Duration.seconds(0));
+    //     rt.setRate(3);
+    //     rt.setCycleCount(18);
+    //     rt.play();
+
+    // double startX = r.getTranslateX();
+    // double startY = r.getTranslateY();
+
+    // Path path = new Path();
+    // path.getElements().add(new MoveTo(startX, startY));
+    // path.getElements().add(new LineTo(endX, endY));
+
+    // PathTransition pathTransition = new PathTransition();
+    // pathTransition.setNode(r);
+    // pathTransition.setPath(path);
+    // pathTransition.setDuration(Duration.seconds(duration));
+    // pathTransition.setCycleCount(reverse ? Animation.INDEFINITE : 1);
+    // pathTransition.setAutoReverse(reverse);
+
+    // SequentialTransition sequentialTransition = new SequentialTransition();
+    // sequentialTransition.getChildren().addAll(
+    //     new PauseTransition(Duration.seconds(delaySeconds)),
+    //     pathTransition
+    // );
+
+    // sequentialTransition.setOnFinished(event -> {
+    //     // Reset the position of the ImageView to its original location
+    //     r.setTranslateX(startX);
+    //     r.setTranslateY(startY);
+
+    //     // Start the animation again
+    //     sequentialTransition.playFromStart();
+    // });
+
+    // sequentialTransition.play();
+    // }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
+        buttonHovered(easyButton, easy, easybox);
+        buttonHovered(mediumButton, medium, mediumbox);
+        buttonHovered(hardButton, hard, hardbox);
+
+        easybox.setVisible(false);
+        mediumbox.setVisible(false);
+        hardbox.setVisible(false);
+        easy.setVisible(false);
+        medium.setVisible(false);
+        hard.setVisible(false);
+        spaceship.setVisible(false);
+        minB2.setVisible(false);
+        minB4.setVisible(false);
+        minB6.setVisible(false);
+        background2.setVisible(false);
+        startButton.setVisible(false);
     }
 }
 
