ca81480feb671f5481fe39a916b75a7b6354aba2
Jay <json941@aucklanduni.ac.nz>
2023-09-21 02:24:47 +1200

    Add keycontrolling method in room2 (tile game)

--- src/main/java/nz/ac/auckland/se206/controllers/TileGameRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/TileGameRoomController.java
@@ -1,21 +1,46 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.AnimationTimer;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 /** Controller class for the room view. */
-public class TileGameRoomController {
+public class TileGameRoomController implements javafx.fxml.Initializable{
+
+  private BooleanProperty wPressed = new SimpleBooleanProperty();
+  private BooleanProperty aPressed = new SimpleBooleanProperty();
+  private BooleanProperty sPressed = new SimpleBooleanProperty();
+  private BooleanProperty dPressed = new SimpleBooleanProperty();
+
+  private BooleanBinding keyPressed = wPressed.or(aPressed).or(sPressed).or(dPressed);
+  private int movementVariable = 5;
+  private double shapesize;
+  private double progressSize = 4.0;
+
+  @FXML private ImageView player;
+  @FXML private Pane scene;
+
+  private double previousX;
+  private double previousY;
 
   @FXML private Rectangle door;
   @FXML private Rectangle window;
@@ -25,9 +50,117 @@ public class TileGameRoomController {
   @FXML private Button btnRoom1;
   @FXML private Button button;
 
-  /** Initializes the room view, it is called when the room loads. */
-  public void initialize() {
+  AnimationTimer timer =
+      new AnimationTimer() {
+        @Override
+        public void handle(long now){
+            
+                previousX = player.getLayoutX(); // Update previousX
+                previousY = player.getLayoutY(); // Update previousY
+
+                if(wPressed.get()){
+                    player.setLayoutY(player.getLayoutY() - movementVariable);
+                }
+                if(aPressed.get()){
+                    player.setLayoutX(player.getLayoutX() - movementVariable);
+                }
+                if(sPressed.get()){
+                    player.setLayoutY(player.getLayoutY() + movementVariable);
+                }
+                if(dPressed.get()){
+                    player.setLayoutX(player.getLayoutX() + movementVariable);
+                }
+                squareBorder();
+        }
+      };
+
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
     // Initialization code goes here
+    shapesize = player.getFitWidth();
+    movementSetup();
+
+    previousX = player.getLayoutX();
+    previousY = player.getLayoutY();
+
+    keyPressed.addListener(
+        ((observableValue, aBoolean, t1) -> {
+          if (!aBoolean) {
+            timer.start();
+          } else {
+            timer.stop();
+          }
+        }));
+
+  }
+
+  @FXML
+  public void movementSetup() {
+    scene.setOnKeyPressed(
+        e -> {
+          if (e.getCode() == KeyCode.W) {
+            wPressed.set(true);
+            System.out.println("w pressed");
+          }
+
+          if (e.getCode() == KeyCode.A) {
+            aPressed.set(true);
+            System.out.println("a pressed");
+          }
+
+          if (e.getCode() == KeyCode.S) {
+            sPressed.set(true);
+            System.out.println("s pressed");
+          }
+
+          if (e.getCode() == KeyCode.D) {
+            dPressed.set(true);
+            System.out.println("d pressed");
+          }
+        });
+
+    scene.setOnKeyReleased(
+        e -> {
+          if (e.getCode() == KeyCode.W) {
+            wPressed.set(false);
+          }
+
+          if (e.getCode() == KeyCode.A) {
+            aPressed.set(false);
+          }
+
+          if (e.getCode() == KeyCode.S) {
+            sPressed.set(false);
+          }
+
+          if (e.getCode() == KeyCode.D) {
+            dPressed.set(false);
+          }
+        });
+  }
+
+  public void squareBorder() {
+    double left = 0;
+    double right = scene.getWidth() - shapesize;
+    double top = 0;
+    double bottom = scene.getHeight() - shapesize;
+
+    if (player.getLayoutX() < left) {
+      player.setLayoutX(left);
+    }
+
+    if (player.getLayoutX() > right) {
+      player.setLayoutX(right);
+    }
+
+    if (player.getLayoutY() < top) {
+      player.setLayoutY(top);
+    }
+
+    if (player.getLayoutY() > bottom) {
+      player.setLayoutY(bottom);
+    }
   }
 
   /**
@@ -122,4 +255,6 @@ public class TileGameRoomController {
   private void back(ActionEvent event) throws IOException {
     App.setScene(AppUi.PLAYER);
   }
+
+  
 }
